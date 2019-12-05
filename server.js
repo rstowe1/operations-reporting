1de46f2432dc2f8f24108912044a9b8ed49e91e6
Ryan <ryanwstowe@gmail.com>
2019-12-05 13:14:51 -0500

    Added server render

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -11,6 +12,16 @@ app.get("/", (req, res) => res.json({ msg: "Welcome to Operations" }));
 
 app.use("/api/approvals", require("./routes/approvals"));
 app.use("/api/status", require("./routes/status"));
+
+// Serve React in productoin
+if (process.env.NODE_ENV === "production") {
+  // Set Static Folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
